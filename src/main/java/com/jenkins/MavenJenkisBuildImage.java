5796869f5fe8fe84abd6cb9a97b377e13abe31e7
Suresh7766 <tirupatisuresh7766@gmail.com>
2022-05-09 21:12:50 +0530

    first commit

---
+++ src/main/java/com/jenkins/MavenJenkisBuildImage.java
@@ -0,0 +1,8 @@
+package com.jenkins;
+
+public class MavenJenkisBuildImage {
+
+       public static void main(String []args){
+              System.out.println("Hello Jenkins Project");
+       }
+}
